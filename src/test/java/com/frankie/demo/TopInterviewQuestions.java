7434aed43bba0d794fb74368a1ae7b470d3c5fce
yycer <15900617310@163.com>
2020-02-15 10:46:49 +0800

    169

--- src/test/java/com/frankie/demo/TopInterviewQuestions.java
+++ src/test/java/com/frankie/demo/TopInterviewQuestions.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author: Yao Frankie
@@ -52,4 +54,74 @@ public class TopInterviewQuestions {
         System.out.println(Arrays.toString(nums));
     }
 
+    /**
+     * Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+     * Input : [3,2,3]
+     * Output: 3
+     */
+    @Test
+    public void question169(){
+        int[] nums = {3, 2, 3};
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num: nums){
+            if (map.get(num) != null){
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+        int result = 0;
+        int count  = 0;
+        for (Map.Entry<Integer, Integer> entry: map.entrySet()){
+            if (entry.getValue() > count){
+                count  = entry.getValue();
+                result = entry.getKey();
+            }
+        }
+    }
+
+    /**
+     * AC: Array and Constant.
+     * https://leetcode.com/problems/majority-element/discuss/51898/AC-clean-Java-solution
+     */
+    @Test
+    public void improve169UsingAC(){
+        int[] nums = {3, 2, 1, 3, 3, 1, 3, 8, 3};
+
+        int n = nums.length, m = nums[0], c = 1;
+        for (int i = 1; i < n; i++){
+            if (nums[i] == m){
+                c++;
+            } else if (c > 0){
+                c--;
+            } else {
+                m = nums[i];
+                c = 1;
+            }
+        }
+
+        System.out.println("m = " + m);
+    }
+
+    @Test
+    public void improve169UsingHashMap(){
+
+        int ret = 0;
+        int[] nums = {3, 2, 3, 2, 3, 1, 3, 8, 3};
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num: nums){
+            if (!map.containsKey(num)){
+                map.put(num, 1);
+            } else {
+                map.put(num, map.get(num) + 1);
+            }
+            // Must be >, eg: [2, 2, 1, 1, 1, 2, 2]
+            if (map.get(num) > nums.length / 2){
+                ret = num;
+                break;
+            }
+        }
+        System.out.println(ret);
+    }
 }
